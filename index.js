3ecfb0c0b79555b972ca9a0daeb615110f5bcaed
Alexis Humberto Solis Martinez <187990204+AlexisSoliss@users.noreply.github.com>
2026-02-11 21:38:59 -0600

    Mejora visual y rediseño de toda la pagina

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const yesBtn = document.querySelector('#yesBtn');
+const noBtn = document.querySelector('#noBtn');
+const preguntaDiv = document.querySelector('#pregunta');
+const mensajeFinalDiv = document.querySelector('#mensajeFinal');
 
-yesBtn.addEventListener('click',function () {
-    alert('Mas te vale mi amor!!!, muamuamuamuamua!')
+// Lógica para el botón SÍ
+yesBtn.addEventListener('click', function () {
+    // Ocultamos la pregunta y los botones
+    preguntaDiv.style.display = 'none';
+    // Mostramos el mensaje final y el gif
+    mensajeFinalDiv.style.display = 'block';
 });
 
-const noBtn = document.querySelector('#noBtn');
-
+// Lógica para el botón NO (huye del mouse)
 noBtn.addEventListener('mouseover', function () {
-    const randomX = parseInt(Math.random()*100);
-    const randomY = parseInt(Math.random()*100);
-    noBtn.style.setProperty('top',randomY+'%');
-    noBtn.style.setProperty('left',randomX+'%');
-    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
-})
+    // Limitamos el movimiento para que no se salga taaaanto de la pantalla (90%)
+    const randomX = Math.random() * 90;
+    const randomY = Math.random() * 90;
+    
+    noBtn.style.top = randomY + '%';
+    noBtn.style.left = randomX + '%';
+    // Esto asegura que el botón se centre en la coordenada
+    noBtn.style.transform = 'translate(-50%, -50%)'; 
+});
